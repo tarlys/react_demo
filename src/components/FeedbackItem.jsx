82509c20a2b8f22fa66f9d3ba97ae906b0cb1b3f
Taras Lysenko <taras.lysenko@ilogos.biz>
2022-02-13 17:21:55 +0200

    New Changes

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,8 @@
-function FeedbackItem() {
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is an example of feedback item</div>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   )
 }
