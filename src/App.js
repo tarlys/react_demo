cb6c7eec6138bf373b785693428a243f6008b2d4
Taras Lysenko <taras.lysenko@ilogos.biz>
2022-12-16 17:07:20 +0200

    add rating select and others

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
-  const [feedback,setFeedback]=useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+    console.log('App', id)
+  }
 
-  return ( 
+  return (
     <>
       <Header />
-      <div className="container">
-        <FeedbackList feedback = {feedback}/>
+      <div className='container'>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,21 @@
-function FeedbackItem({ item }) {
+import { FaTimes } from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from './shared/Card'
+
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className="card">
-      <div className="num-display">{item.rating}</div>
-      <div className="text-display">{item.text}</div>
-    </div>
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
 }
 
 export default FeedbackItem
