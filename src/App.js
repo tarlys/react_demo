23750949e84e9105dff4b62befb059ecc7057678
Taras Lysenko <taras.lysenko@ilogos.biz>
2022-12-22 00:51:18 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import Header from './components/Header'
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
-// import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
 import { FeedbackProvider } from './context/FeedbackContext'
-import FeedbackData from './data/FeedbackData'
-// import Card from './components/shared/Card'
 import Post from './components/Post'
 
 function App() {
