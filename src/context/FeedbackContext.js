8bb63966c577a57b6fd19062af3e495e45fe6b3c
Taras Lysenko <taras.lysenko@ilogos.biz>
2022-12-22 00:38:26 +0200

    create feedback app

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,71 @@
+import { createContext, useState } from 'react'
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This is feedback from context 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback from context 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback from context 3',
+      rating: 7,
+    },
+  ])
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+  //Edit feedback
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
+  //Delete feedback
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+    console.log(feedback)
+  }
+
+  //Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = feedback.length + 1
+    console.log(newFeedback)
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        feedbackEdit,
+        editFeedback,
+        updateFeedback,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  )
+}
+
+export default FeedbackContext
